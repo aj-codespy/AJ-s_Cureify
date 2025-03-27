5e3f8057fe012036dc3156b7c050a1ebb6bad8bc
Ayush Jha <145047690+aj-codespy@users.noreply.github.com>
2025-03-27 19:02:42 +0530

    Update and rename sapp.py to app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st 
+from PIL import Image
 from main import mainAgent
 
 st.title('Cureify: Clinical Decision Support System.')
 
 prompt = st.input('Enter your prompt')
 img = st.file_uploader('Upload an image')
-
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    
 result = mainAgent(prompt, img)
 
 if st.button('Submit'):
